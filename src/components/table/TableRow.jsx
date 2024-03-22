63b8f6786cecd11869e6f4a5fc1f3122c0ad0c5e
trees543 <76708281+trees543@users.noreply.github.com>
2024-03-22 16:26:05 -0500

    Finalized some table styles.  Button shows up in no-row situation.
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Josh Lakenan <joshualakenan@gmail.com>

--- src/components/table/TableRow.jsx
+++ src/components/table/TableRow.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import TableCell from "./TableCell";
 import Checkbox from "../utils/Checkbox.jsx";
 
@@ -11,16 +13,23 @@ export default function TableRow({
   setSelectedRow,
   tableIsScrolled,
 }) {
+  const [hovering, setHovering] = useState(false);
+
   const {
     actionCreators: { editRecord },
   } = useModalContext();
 
   return (
     table && (
-      <tr className="tr" onClick={() => editRecord({ table, row, setRows })}>
+      <tr
+        className={`tr ${hovering ? "hovering" : ""}`}
+        onClick={() => editRecord({ table, row, setRows })}
+        onMouseOver={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      >
         <td
-          className={`select-row-header sticky-col ${
-            tableIsScrolled && "shadow"
+          className={`sticky-col ${tableIsScrolled ? "shadow" : ""} ${
+            hovering ? "hovering" : ""
           }`}
         >
           <Checkbox
@@ -59,7 +68,9 @@ export default function TableRow({
           column={{ type: "updated_at", name: "updated_at" }}
           row={row}
         />
-        <td className="sticky-col right-arrow">{`->`}</td>
+        <td
+          className={`sticky-col right-arrow ${hovering ? "hovering" : ""}`}
+        >{`->`}</td>
       </tr>
     )
   );
