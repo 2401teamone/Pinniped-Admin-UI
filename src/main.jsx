7bef4de279e813612f7aa7331eab53dea2857315
trees543 <76708281+trees543@users.noreply.github.com>
2024-03-28 20:45:25 -0500

    Added ErrorBoundary component to catch all errors
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Josh Lakenan <joshualakenan@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,22 @@ import App from "./App.jsx";
 import "./assets/reset.css";
 import "./assets/index.css";
 
+import ErrorBoundary from "./components/utils/ErrorBoundary.jsx";
 import { ModalProvider } from "./hooks/useModal.jsx";
 import { NotificationProvider } from "./hooks/useNotifications.jsx";
 import { ConfirmModalProvider } from "./hooks/useConfirmModal.jsx";
 import { AuthProvider } from "./hooks/useAuth.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <NotificationProvider>
-    <AuthProvider>
-      <ConfirmModalProvider>
-        <ModalProvider>
-          <App />
-        </ModalProvider>
-      </ConfirmModalProvider>
-    </AuthProvider>
-  </NotificationProvider>
+  <ErrorBoundary>
+    <NotificationProvider>
+      <AuthProvider>
+        <ConfirmModalProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </ConfirmModalProvider>
+      </AuthProvider>
+    </NotificationProvider>
+  </ErrorBoundary>
 );
